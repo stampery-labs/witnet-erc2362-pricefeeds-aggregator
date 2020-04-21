e5e38b983f7aaf48967c3dba82a0081ddc73aa3a
Gorka Irazoqui <gorka.irazoki@gmail.com>
2020-04-21 09:24:11 +0200

    feat: add flattened and push address upon migration

--- migrations/2_witnet_price_feeds.js
+++ migrations/2_witnet_price_feeds.js
@@ -1,5 +1,15 @@
-const WitnetPriceFeeds = artifacts.require("WitnetPriceFeeds");
+const addresses = require("./addresses.json")
+const WitnetPriceFeeds = artifacts.require("WitnetPriceFeedsAggregator");
 
-module.exports = function(deployer) {
-  deployer.deploy(WitnetPriceFeeds);
+module.exports = function(deployer, network) {
+  network = network.split("-")[0]
+  deployer.deploy(WitnetPriceFeeds)
+    .then(() => WitnetPriceFeeds.deployed())
+    .then((instance) => {
+      if (network in addresses) {
+        for (var key in addresses[network]) {
+         instance.registerFeed(addresses[network][key].id, addresses[network][key].address)
+        }
+      }
+   })
 };
